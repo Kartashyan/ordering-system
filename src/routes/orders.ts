6165ecff7af8364b5290348be0ea91a67ae7e291
Eliseus <yeghishe.kartashyan@gmail.com>
2024-04-04 19:03:49 +0200

    remove code

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,44 +1,11 @@
 import { Router } from "express";
 const router = Router();
-import { createOrder, updateOrderStatus } from "../services/orders";
-import { OrderRequestSchema, OrderStatusSchema } from "../core/orders";
 
-router.post("/", async (req, res) => {
-  try {
-    const result = OrderRequestSchema.safeParse(req.body);
-    if (!result.success) {
-      return res.status(400).json(result.error.format());
-    }
-    const validatedData = result.data;
-    const order = await createOrder(validatedData);
-    res.status(201).json(order);
-  } catch (error) {
-    console.error("Error creating order:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.post("/", async (req, res) => {});
 
 router.get("/:orderId", (req, res) => {});
 
-router.patch("/:orderId", async (req, res) => {
-  // Implementation for updating an order's status
-  try {
-    // Validate the request body
-    const result = OrderStatusSchema.safeParse(req.body);
-    if (!result.success) {
-      return res.status(400).json(result.error.format());
-    }
-    const validatedData = result.data;
-    const updatedOrder = await updateOrderStatus(
-      req.params.orderId,
-      validatedData
-    );
-    res.json(updatedOrder);
-  } catch (error) {
-    console.error("Error updating order status:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.patch("/:orderId", async (req, res) => {});
 
 router.get("/", (req, res) => {});
 
