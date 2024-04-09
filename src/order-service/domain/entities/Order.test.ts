7f8cd8e9676dffea3eaddd961576500e610fd6e3
Eliseus <yeghishe.kartashyan@gmail.com>
2024-04-09 17:35:59 +0200

    add tests for Order entity

---
+++ src/order-service/domain/entities/Order.test.ts
@@ -0,0 +1,39 @@
+import { describe, it, expect } from "vitest";
+import { Order, OrderItemProps } from "./Order";
+import { StatusStateManager } from "./OrderStatusManager";
+
+describe("Order", () => {
+  let order: Order;
+  const items: OrderItemProps[] = [
+    { id: 1, quantity: 2 },
+    { id: 2, quantity: 1 },
+  ];
+
+  it("should create an order with the provided items, id, and status", () => {
+    const order = Order.create(items, "123", "Pending");
+    expect(order.id).toBe("123");
+    expect(order.status).toBe("Pending");
+    expect(order.items).toEqual(items);
+  });
+
+  it("should change the status of the order if transition is valid", () => {
+    const order = Order.create(items, "123", "Pending");
+    const nextState = StatusStateManager.getNextState(order.status);
+    order.changeStatusTo(nextState);
+    expect(order.status).toBe("InPreparation");
+  });
+  it("should throw an error if the status is invalid", () => {
+    const order = Order.create(items, "123", "Pending");
+    expect(() => order.changeStatusTo("invalid" as any)).toThrowError();
+  });
+  it("should throw an error if the status is the final state", () => {
+    const order = Order.create(items, "123", "Completed");
+    expect(() =>
+      order.changeStatusTo(StatusStateManager.getNextState(order.status))
+    ).toThrowError();
+  });
+  it("should throw an error if the status transition is not valid", () => {
+    const order = Order.create(items, "123", "InPreparation");
+    expect(() => order.changeStatusTo("Pending")).toThrowError();
+  });
+});
