3deb0c81351a9ae3e4ef21befb7ac983d8184283
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-09 19:17:41 +0200

    add product name value object

---
+++ src/orders/domain/product-name.value-object.ts
@@ -0,0 +1,17 @@
+import { ValueObject } from "../../shared/lib";
+
+export class ProductName extends ValueObject<{ value: string }> {
+    public readonly value: string;
+
+    private constructor(value: string) {
+        super({ value });
+        this.value = value;
+    }
+
+    public static create(value: string): ProductName {
+        if (value.length < 2) {
+            throw new Error(`Product name <${value}> should have at least 2 characters`);
+        }
+        return new ProductName(value);
+    }
+}
