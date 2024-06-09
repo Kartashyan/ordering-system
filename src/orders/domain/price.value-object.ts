e7b4f0cdf305e7fd291405243a8da8081bb30e5d
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-09 19:18:09 +0200

    add price value object

---
+++ src/orders/domain/price.value-object.ts
@@ -0,0 +1,21 @@
+import { ValueObject } from "../../shared/lib";
+
+interface PriceProps {
+  value: number;
+}
+
+export class Price extends ValueObject<PriceProps> {
+  public readonly value: number;
+
+  private constructor(value: number) {
+    super({ value });
+    this.value = value;
+  }
+
+  public static create(value: number): Price {
+    if (value < 0) {
+      throw new Error(`Price <${value}> should be positive`);
+    }
+    return new Price(value);
+  }
+}
