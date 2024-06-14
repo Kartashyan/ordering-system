c89a7e86a169a1150275ecdccf35a6eb50b63679
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-14 09:34:30 +0200

    temp comment out event addition in order aggragate

--- src/orders/domain/order.aggregate.ts
+++ src/orders/domain/order.aggregate.ts
@@ -1,4 +1,3 @@
-import { i } from "vitest/dist/reporters-P7C2ytIv";
 import { Aggregate, ID, Result, UID } from "../../shared/lib";
 import { OrderStatus, OrderStatuses, StatusStateManager } from "./entities/OrderStatusManager";
 import { OrderItem, OrderItemProps } from "./order-item.value-object";
@@ -16,26 +15,35 @@ export class Order extends Aggregate<OrderProps> {
     }
 
     public get items(): OrderItemProps[] {
-        return this.props.items;
+        return this.props.items.map((item) => ({
+            productId: item.productId,
+            quantity: item.quantity,
+        }));
     }
 
     public get status(): string {
         return this.props.status;
     }
 
+    public getId(): string {
+        return this.props.id.value();
+    }
+
     public static create(
-        items: OrderItem[],
+        items: OrderItemProps[],
         id?: UID,
         status?: string
     ): Result<Order> {
-        const order = new Order({ id: id || ID.create(), status: status || OrderStatuses.Pending, items });
+        const orderItems = items.map((item) => OrderItem.create(item.productId, item.quantity).value());
+
+        const order = new Order({ id: id || ID.create(), status: status || OrderStatuses.Pending, items: orderItems});
         if (order.items.length <= 0) {
             Result.fail("Order should have at least one item");
         }
         const isNewOrder = !id;
-        if (isNewOrder) {
-            order.addEvent(new OrderCreatedEvent());
-        }
+        // if (isNewOrder) {
+        //     order.addEvent(new OrderCreatedEvent());
+        // }
         return Result.Ok(order);
     }
 
