73cf77d107a68de0be1ba5696428b475aeced0dd
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-15 17:01:11 +0200

    fix domain evnents

--- src/shared/lib/core/events.ts
+++ src/shared/lib/core/events.ts
@@ -1,48 +1,27 @@
-import { DEvent, Handler, Options, PromiseHandler } from "../types";
+import { DEvent } from "../types";
 
 export class DomainEvents<T> {
   private _events: DEvent<T>[];
 
   constructor(private readonly aggregate: T) {
     this._events = [];
+    this.aggregate = aggregate;
   }
 
-  private getPriority(): number {
-    const totalEvents = this._events.length;
-    if (totalEvents <= 1) return 2;
-    return totalEvents;
-  }
 
-  dispatchEvent(eventName: string, ...args: any[]): void | Promise<void> {
+  dispatchEvent(eventName: string): void | Promise<void> {
     const _event = this._events.find(
       (evt): boolean => evt.eventName === eventName,
     );
+
     if (!_event) return;
-    _event.handler(this.aggregate, [_event, ...args]);
     this.removeEvent(eventName);
   }
 
-  private getDefaultOptions(): Options {
-    const priority = this.getPriority();
-    return {
-      priority,
-    };
-  }
-
-  addEvent(eventName: string, handler: Handler<T>, options?: Options): void {
-    const defaultOptions = this.getDefaultOptions();
-    const opt = options ? options : defaultOptions;
+  addEvent(eventName: string): void {
     this.validateEventName(eventName);
-    this.validateHandler(handler, eventName);
     this.removeEvent(eventName);
-    this._events.push({ eventName, handler, options: opt });
-  }
-
-  private validateHandler(handler: Handler<T>, eventName: string): void {
-    if (typeof handler !== "function") {
-      const message = `addEvent: handler for ${eventName} is not a function`;
-      throw new Error(message);
-    }
+    this._events.push({ eventName, aggregate: this.aggregate});
   }
 
   private validateEventName(eventName: string): void {
@@ -63,18 +42,7 @@ export class DomainEvents<T> {
   }
 
   async dispatchEvents(): Promise<void> {
-    const promisesEvents: PromiseHandler<T>[] = [];
-    const sorted = this._events.sort(
-      (a, b): number => a.options.priority - b.options.priority,
-    );
-    sorted.forEach((_event): void => {
-      const fn = _event.handler(this.aggregate, [_event]);
-      if (fn instanceof Promise) {
-        promisesEvents.push(fn as unknown as PromiseHandler<T>);
-      }
-    });
-    await Promise.all(promisesEvents).catch(console.error);
-    this.clearEvents();
+
   }
 }
 
