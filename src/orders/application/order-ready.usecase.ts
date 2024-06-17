6cdaaae1f3db9b7e653e5c1a60b89a7ad38a6e2a
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-17 18:38:44 +0200

    refactor: Update OrderReadyUsecase to use Result class for error handling
    The OrderReadyUsecase has been updated to utilize the Result class for error handling. This change improves the consistency and readability of the code by using a standardized approach for handling success and failure scenarios.

--- src/orders/application/order-ready.usecase.ts
+++ src/orders/application/order-ready.usecase.ts
@@ -1,34 +1,31 @@
 import { DomainEvents } from "../../shared/DomainEvents";
-import { OrderRepository } from "../domain/ports/OrderRepositoryInterface";
+import { OrderRepository } from "../domain/ports/order.repo-port";
 import { OrderReadyEvent } from "../domain/events/order-ready.event";
 import { orderRepository } from "../infra/order-prisma.repo-adapter";
-
-type Success = { success: true };
-type Failure = { success: false; reason: string };
-type UsecaseResponse = Success | Failure;
+import { Result } from "../../shared/lib";
 
 export class OrderReadyUsecase {
   private orderRepository: OrderRepository;
   constructor(orderRepository: OrderRepository) {
     this.orderRepository = orderRepository;
   }
-  async execute(orderData: { id: string }): Promise<UsecaseResponse> {
+  async execute(orderData: { id: string }): Promise<Result<void>> {
     const order = await this.orderRepository.find(orderData.id);
     if (!order) {
-      return { success: false, reason: "Order not found" };
+      return Result.fail("Order not found");
     }
     try {
       order.changeStatusTo("ReadyForPickup");
     } catch (e: unknown) {
       if (e instanceof Error) {
-        return { success: false, reason: e.message };
+        return Result.fail(e.message);
       } else {
-        return { success: false, reason: "Failed to update order status" };
+        return Result.fail("An error occurred");
       }
     }
     await this.orderRepository.save(order);
-    DomainEvents.publishEvent(new OrderReadyEvent(orderData.id));
-    return { success: true };
+    DomainEvents.publishEvent(new OrderReadyEvent(order));
+    return Result.Ok();
   }
 }
 
