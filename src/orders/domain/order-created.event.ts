6792ba662b9044eca66cb74f77efa59152888776
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-18 00:43:26 +0200

    refactor: Update OrderCreatedEvent to use occurredOn property
    The OrderCreatedEvent class has been updated to include the occurredOn property, which represents the date and time when the event occurred. This change improves the consistency and completeness of the event implementation.

--- src/orders/domain/order-created.event.ts
+++ src/orders/domain/order-created.event.ts
@@ -1,12 +1,11 @@
 import { DomainEvent } from "../../shared/lib/core/domain-event";
 import { Order } from "./order.aggregate";
 
-export class OrderCreatedEvent implements DomainEvent<Order>{
-	eventName: string;
-	aggregate: Order;
-	constructor(order: Order) {
-		this.eventName = "Orders:OrderCreatedEvent";
-		this.aggregate = order;
+export class OrderCreatedEvent implements DomainEvent {
+	eventName: string = "Orders:OrderCreatedEvent";;
+	occurredOn: Date;
+	constructor(public aggregateId: string) {
+		this.occurredOn = new Date();
 	}
 }
 
