f1186356dbc2be8a1c8568121f99c8c92c4235c3
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-18 21:53:01 +0200

    refactor: Update StatusTransitionFailedEvent constructor parameter name
    The constructor parameter "attemptedStatus" in the StatusTransitionFailedEvent class has been renamed to "newStatus" for clarity and consistency with the codebase. This change improves the readability and maintainability of the code.

--- src/orders/domain/events/wrong-status-transition.event.ts
+++ src/orders/domain/events/wrong-status-transition.event.ts
@@ -3,7 +3,8 @@ import { DomainEvent } from "../../../shared/lib/core/domain-event";
 export class StatusTransitionFailedEvent implements DomainEvent {
     eventName = "StatusTransitionFailed";
     occurredOn: Date;
-    constructor(public aggregateId: string, public oldStatus: string, public newStatus: string) {
+    reason = "Invalid status transition";
+    constructor(public aggregateId: string, public oldStatus: string, public attemptedStatus: string) {
         this.occurredOn = new Date();
     }
 }
