ec9bbf684df343dcce587e1a06237567f76b62b2
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-19 18:17:40 +0200

    refactor: Update kitchen route to enqueue orders immediately

--- src/infra/routes/kitchen.ts
+++ src/infra/routes/kitchen.ts
@@ -2,10 +2,10 @@ import { Router } from "express";
 import { getNextOrderController } from "../../orders/infra/get-next-order.controller";
 import { kitchenQueue } from "../../orders/infra/kitchen.queue";
 import { orderReadyController } from "../../orders/infra/order-ready.controller";
-import { DomainEvents } from "../../shared/DomainEvents";
+import { LocalEventManager } from "../../shared/DomainEvents";
 const router = Router();
 
-DomainEvents.subscribeToEvent(
+LocalEventManager.subscribeToEvent(
   "order-created",
   (payload: any) => {
     console.log("Order created", payload);
@@ -29,10 +29,10 @@ router.get("/events", (req, res) => {
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
 
-  DomainEvents.subscribeToEvent(
+  LocalEventManager.subscribeToEvent(
     "order-created",
     (payload: any) => {
-      console.log("Order created", payload);
+      console.log("New Order has been sent to the kitchen client", payload);
       // send a nodification to the kitchen (server sent events)
       res.write(`data: ${JSON.stringify(payload)}`);
     }
