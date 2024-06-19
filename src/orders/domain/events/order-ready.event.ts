6a97d20875353d6ef6f3e91310a914cf7123243e
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-19 18:18:46 +0200

    refactor: Update OrderReadyEvent constructor to accept aggregateId
    This commit updates the constructor of the OrderReadyEvent class in order-ready.event.ts to accept the aggregateId as a parameter. This change improves the clarity and consistency of the code by explicitly specifying the required input for creating an instance of the event.

--- src/orders/domain/events/order-ready.event.ts
+++ src/orders/domain/events/order-ready.event.ts
@@ -3,7 +3,8 @@ import { DomainEvent } from "../../../shared/lib/core/domain-event";
 export class OrderReadyEvent implements DomainEvent {
   readonly eventName: string = "order-ready";
   readonly occurredOn: Date;
-  constructor(readonly aggregateId: string) {
+  readonly aggregateId: string
+  constructor(aggregateId: string) {
     this.occurredOn = new Date();
     this.aggregateId = aggregateId;
   }
