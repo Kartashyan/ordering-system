11eca0c1c92f145e96a22852b598131eb6ebc8ab
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-19 18:19:21 +0200

    refactor: Update DomainEvents to use LocalEventManager
    The DomainEvents class in DomainEvents.ts has been renamed to LocalEventManager to better reflect its purpose. This change ensures compatibility with the latest version of the codebase and improves the clarity and consistency of the code.

--- src/shared/DomainEvents.ts
+++ src/shared/DomainEvents.ts
@@ -1,12 +1,12 @@
 import { EventEmitter } from "events";
 import { DomainEvent } from "./lib/core/domain-event";
 
-export class DomainEvents {
+export class LocalEventManager {
   private static events: DomainEvent[] = [];
   private static emitter = new EventEmitter();
   static publishEvent(event: DomainEvent): void {
     this.events.push(event);
-    this.emitter.emit(event.eventName, event.payload);
+    this.emitter.emit(event.eventName, event);
   }
   static subscribeToEvent(eventType: string, callback: (payload: any) => void) {
     this.emitter.on(eventType, callback);
