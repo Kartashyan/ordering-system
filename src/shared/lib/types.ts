9fcaf1b2ba3761d3e95d689bd1ad4d7c76efc43b
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-20 21:54:59 +0200

    refactor: Remove unused methods and improve code clarity in ID class

--- src/shared/lib/types.ts
+++ src/shared/lib/types.ts
@@ -1,25 +1,7 @@
 
-export interface UID<T = string> {
-	value(): string;
-	isNew(): boolean;
-	createdAt(): Date;
-	equal(id: UID<string>): boolean;
-	isEqual(id: UID<string>): boolean;
-	deepEqual(id: UID<string>): boolean;
-}
-
 export interface ICommand<A, B> {
 	execute(data: A): B;
 }
-
-export type Unit = 'minute' | 'hour' | 'day' | 'week' | 'month' | 'year';
-
-export const ONE_MINUTE = 60000;
-export const ONE_HOUR = ONE_MINUTE * 60;
-export const ONE_DAY = ONE_HOUR * 24;
-export const ONE_WEEK = ONE_DAY * 7;
-export const ONE_MONTH = ONE_DAY * 30;
-export const ONE_YEAR = ONE_DAY * 365;
 
 export interface DEvent<T> {
 	eventName: string;
