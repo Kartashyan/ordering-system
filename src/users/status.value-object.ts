140d6aa1b7206ae98fb16c8c4f31d45706f027de
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-20 23:57:08 +0200

    feat: Add Status value object for user status

---
+++ src/users/status.value-object.ts
@@ -0,0 +1,19 @@
+import { ValueObject } from "../shared/lib";
+
+export class Status extends ValueObject<string> {
+  private constructor(status: string) {
+    super(status);
+  }
+
+  public static create(status: string): Status {
+    if (!Status.isValid(status)) {
+      throw new Error("Invalid status");
+    }
+
+    return new Status(status);
+  }
+
+  public static isValid(status: string): boolean {
+    return ["active", "inactive"].includes(status);
+  }
+}
