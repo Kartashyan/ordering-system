cc0c17ef0e966209fbe0ef27813bf2dbf0f803ff
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-20 21:55:35 +0200

    refactor: Update ID class to remove unused methods and improve code clarity

--- src/shared/lib/core/id.ts
+++ src/shared/lib/core/id.ts
@@ -1,11 +1,9 @@
 import { randomUUID } from './crypto';
-import { UID } from "../types";
 
-export class ID<T = string> implements UID<T> {
+export class ID<T = string> {
 	private _value: string;
 	private _isNew: boolean;
 	private _createdAt: Date;
-	private readonly MAX_SIZE: number = 16;
 
 	private constructor(id?: T) {
 		this._createdAt = new Date();
@@ -33,30 +31,32 @@ export class ID<T = string> implements UID<T> {
 		return this._createdAt;
 	}
 
-	equal(id: UID<any>): boolean {
+	equal(id: ID<any>): boolean {
 		return (typeof this._value === typeof id?.value()) && (this._value as any === id?.value());
 	}
 
-	isEqual(id: UID<any>): boolean {
+	isEqual(id: ID<any>): boolean {
 		return this.equal(id);
 	}
 
-	deepEqual(id: UID<any>): boolean {
+	deepEqual(id: ID<any>): boolean {
 		const A = JSON.stringify(this);
 		const B = JSON.stringify(id);
 		return A === B;
 	}
 
-	clone(): UID<T> {
-		return new ID(this._value) as unknown as UID<T>;
+	clone(): ID<T> {
+		return new ID(this._value) as unknown as ID<T>;
 	}
 
-	public static create<T = string | number>(id?: T): UID<string> {
-		return new ID(id) as unknown as UID<string>;
+	public static create<T = string | number>(id?: T): ID<string> {
+		return new ID(id) as unknown as ID<string>;
 	}
 }
 
 export default ID;
 export const id = ID;
 export const Uid = ID.create;
+
 export const Id = ID.create;
+
