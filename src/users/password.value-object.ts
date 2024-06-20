0b28dc8421a55094b88b4b6532a4ddd4a170873c
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-20 23:58:02 +0200

    feat: Add Password value object for user password

---
+++ src/users/password.value-object.ts
@@ -0,0 +1,19 @@
+import { ValueObject } from "../shared/lib";
+
+export class Password extends ValueObject<string> {
+  private constructor(password: string) {
+    super(password);
+  }
+
+  public static create(password: string): Password {
+    if (!Password.isValid(password)) {
+      throw new Error("Invalid password");
+    }
+
+    return new Password(password);
+  }
+
+  public static isValid(password: string): boolean {
+    return password.length >= 8;
+  }
+}
