e47bf5854daa5228db958ffb489144da53ba54df
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-20 23:58:16 +0200

    feat: Add Role value object for user role

---
+++ src/users/role.value-object.ts
@@ -0,0 +1,19 @@
+import { ValueObject } from "../shared/lib";
+
+export class Role extends ValueObject<string> {
+  private constructor(role: string) {
+    super(role);
+  }
+
+  public static create(role: string): Role {
+    if (!Role.isValid(role)) {
+      throw new Error("Invalid role");
+    }
+
+    return new Role(role);
+  }
+
+  public static isValid(role: string): boolean {
+    return role === "admin" || role === "user";
+  }
+}
