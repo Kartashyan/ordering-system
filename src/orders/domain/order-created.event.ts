12d6c3b879e29af4bdec28b8220fa6afd50b56b3
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-22 12:53:52 +0200

    refactor: Update import statement for DomainEvent in order-created.event.ts

--- src/orders/domain/order-created.event.ts
+++ src/orders/domain/order-created.event.ts
@@ -1,4 +1,4 @@
-import { DomainEvent } from "../../shared/lib/core/domain-event";
+import { DomainEvent } from "../../shared/core/domain-event";
 
 export class OrderCreatedEvent implements DomainEvent {
 	eventName: string = "order-created";
