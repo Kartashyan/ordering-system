c628cb32690d8b7e4fc29d78ebaccdff60cbbf09
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-22 12:55:21 +0200

    refactor: Update import statements for DomainEvent and ValueObject
    This commit updates the import statements for the DomainEvent and ValueObject modules in multiple files. The changes ensure consistency in the import paths and align with recent refactorings in the project structure.

---
+++ src/shared/core/entity.ts
@@ -0,0 +1,36 @@
+import { ID } from "./id";
+
+const isEntity = (v: any): v is Entity<any> => {
+	return v instanceof Entity;
+};
+export class Entity<Props> {
+	protected id: ID;
+	public readonly props: Props;
+	constructor(props: Props, id?: ID) {
+		this.id = id ? id : ID.create();
+		this.props = props;
+	}
+
+	public equals(other?: Entity<Props>): boolean {
+
+		if (other == null || other == undefined) {
+			return false;
+		}
+
+		if (this === other) {
+			return true;
+		}
+
+		if (!isEntity(other)) {
+			return false;
+		}
+
+		return this.id.isEqual(other.id);
+	}
+
+	isNew(): boolean {
+		return this.id.isNew();
+	}
+}
+
+export default Entity;
