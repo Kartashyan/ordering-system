6b4b1858bf6892046b2dcb8ec80d8a427f130c3f
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-22 12:54:57 +0200

    refactor: Update import statements for DomainEvent and ValueObject
    This commit updates the import statements for the DomainEvent and ValueObject modules in multiple files. The changes ensure consistency in the import paths and align with recent refactorings in the project structure.

---
+++ src/shared/core/aggregate.ts
@@ -0,0 +1,44 @@
+import { EventManager } from "./event-manager";
+import Context from "./context";
+import Entity from "./entity";
+import DomainEvents from "./events";
+import ID from "./id";
+import { DomainEvent } from "./domain-event";
+
+export class Aggregate<Props> extends Entity<Props> {
+	private _domainEvents: DomainEvents<this>;
+
+	constructor(props: Props, id?: ID) {
+		super(props, id);
+		this._domainEvents = new DomainEvents(this);
+	}
+
+	public context(): EventManager {
+		return Context.events();
+	}
+
+	dispatchEvent<E>(eventName: string, eventBody: E): void | Promise<void> {
+		this._domainEvents.dispatchEvent(eventName, eventBody);
+	}
+
+	async dispatchAll(): Promise<void> {
+		await this._domainEvents.dispatchEvents();
+	};
+
+	clearEvents(): void {
+		this._domainEvents.clearEvents();
+	};
+
+	addEvent(event: DomainEvent): void {
+		this._domainEvents.addEvent(event);
+	}
+
+	deleteEvent(eventName: string): void {
+		this._domainEvents.removeEvent(eventName);;
+	}
+
+	toObject(): Props {
+		return this.props;
+	}
+}
+export default Aggregate;
