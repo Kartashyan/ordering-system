b57128d8f4c41c2151aa03a3cedf855bbdd55bd5
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-22 12:55:46 +0200

    refactor: Update import statements for DomainEvent and ValueObject
    This commit updates the import statements for the DomainEvent and ValueObject modules in multiple files. The changes ensure consistency in the import paths and align with recent refactorings in the project structure.

---
+++ src/shared/core/result.ts
@@ -0,0 +1,98 @@
+/**
+ * @summary The result is used to returns a operation result instead the own value.
+ * @interface IResult<T, D>;
+ * @classdesc on `T` refer to type of the value and `D` type of the error.
+ * @default D is string.
+ */
+
+export interface IResultObject<T, D> {
+	isOk: boolean;
+	isFail: boolean;
+	data: T | null;
+	error: D | null;
+}
+
+export interface IResult<T, D = string, M = {}> {
+	value(): T;
+	error(): D;
+	isFail(): boolean;
+	isOk(): boolean;
+	toObject(): IResultObject<T, D>;
+}
+
+export class Result<T = void, D = string> implements IResult<T, D> {
+
+	#isOk: Readonly<boolean>;
+	#isFail: Readonly<boolean>;
+	#data: Readonly<T | null>;
+	#error: Readonly<D | null>;
+
+	private constructor(isSuccess: boolean, data?: T, error?: D) {
+		this.#isOk = isSuccess;
+		this.#isFail = !isSuccess;
+		this.#data = data ?? null;
+		this.#error = error ?? null;
+	}
+
+	public static Ok(): Result<void>;
+
+	public static Ok(): IResult<void>;
+
+	public static Ok<T, D = string>(data: T): Result<T, D>;
+
+	public static Ok<T, D = string>(data: T): IResult<T, D>;
+
+	public static Ok<T, D = string>(data?: T): IResult<T, D> {
+		const _data = typeof data === 'undefined' ? null : data;
+		const ok = new Result(true, _data, null) as unknown as IResult<T, D>;
+		return Object.freeze(ok) as IResult<T, D>;
+	}
+
+	public static fail<D = string, T = void>(error?: D): Result<T, D>;
+
+	public static fail<D = string, T = void>(error?: D): IResult<T, D> {
+		const _error = typeof error !== 'undefined' && error !== null ? error : 'void error. no message!';
+		const fail = new Result(false, null, _error) as unknown as IResult<T, D>;
+		return Object.freeze(fail) as IResult<T, D>;
+	}
+
+	public static combine<A = any, B = any, M = any>(results: Array<IResult<any, any, any>>) {
+		const _results = results.filter((result) => result.isFail());
+		if (_results.length > 0) {
+			const errors = _results.map((result) => result.error());
+			return Result.fail(errors);
+		}
+		return Result.Ok();
+	}
+
+	value(): T {
+		return this.#data as T;
+	}
+
+	error(): D {
+		return this.#error as D;
+	}
+
+	isFail(): boolean {
+		return this.#isFail;
+	}
+
+	isOk(): boolean {
+		return this.#isOk;
+	}
+
+	toObject(): IResultObject<T, D> {
+		const metaData = {
+			isOk: this.#isOk,
+			isFail: this.#isFail,
+			data: this.#data as T | null,
+			error: this.#error as D | null,
+		}
+
+		return Object.freeze(metaData);
+	}
+}
+
+export default Result;
+
+
