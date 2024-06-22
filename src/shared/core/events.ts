d9341be60bba7cbccbc8f44115dac439668afc3f
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-22 12:55:32 +0200

    refactor: Update import statements for DomainEvent and ValueObject
    This commit updates the import statements for the DomainEvent and ValueObject modules in multiple files. The changes ensure consistency in the import paths and align with recent refactorings in the project structure.

---
+++ src/shared/core/events.ts
@@ -0,0 +1,54 @@
+import { LocalEventManager } from "../DomainEvents";
+import { DomainEvent } from "./domain-event";
+
+export class DomainEvents<T> {
+  private _events: DomainEvent[];
+
+  constructor(private readonly aggregate: T) {
+    this._events = [];
+    this.aggregate = aggregate;
+  }
+
+
+  dispatchEvent<E>(eventName: string, eventBody?: E): void | Promise<void> {
+    const _event = this._events.find(
+      (evt): boolean => evt.eventName === eventName,
+    );
+    console.log(`EVENT DISPATCH: ${eventName}, ${eventBody}`);
+    if (!_event) return;
+    LocalEventManager.publishEvent(_event);
+    this.removeEvent(eventName);
+  }
+
+  addEvent(event: DomainEvent): void {
+    this.validateEventName(event.eventName);
+    this.removeEvent(event.eventName);
+    this._events.push(event);
+  }
+
+  private validateEventName(eventName: string): void {
+    if (typeof eventName !== "string" || String(eventName).length < 3) {
+      const message = `addEvent: invalid event name ${eventName}`;
+      throw new Error(message);
+    }
+  }
+
+  clearEvents(): void {
+    this._events = [];
+  }
+
+  removeEvent(eventName: string): void {
+    this._events = this._events.filter(
+      (event): boolean => event.eventName !== eventName,
+    );
+  }
+
+  async dispatchEvents(): Promise<void> {
+    for (const event of this._events) {
+      LocalEventManager.publishEvent(event);
+    }
+    this.clearEvents();
+  }
+}
+
+export default DomainEvents;
