98b3d8722212a1425d0f9939108d6580cb90ed9a
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-22 12:55:55 +0200

    refactor: Update import statements for DomainEvent and ValueObject

---
+++ src/shared/core/value-object.ts
@@ -0,0 +1,28 @@
+export class ValueObject<Props>{
+	protected props: Props;
+	constructor(props: Props) {
+		this.props = props;
+	}
+
+	isEqual(other: this): boolean {
+		const props = this.props;
+		const otherProps = other?.props;
+
+		if (other === null || other === undefined) {
+			return false;
+		  }
+		  if (other.props === undefined) {
+			return false;
+		  }
+
+		const stringifyAndOmit = (obj: any): string => {
+			if (!obj) return '';
+			const { createdAt, updatedAt, ...cleanedProps } = obj;
+			return JSON.stringify(cleanedProps);
+		};
+
+		return stringifyAndOmit(props) === stringifyAndOmit(otherProps);
+	}
+}
+
+export default ValueObject;
