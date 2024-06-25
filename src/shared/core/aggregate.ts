d3d0815511640d9f9b089eb93c6bce6e1f7a47ae
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-25 23:23:03 +0200

    refactor: Update Aggregate class to use EventEmitter for event dispatching

--- src/shared/core/aggregate.ts
+++ src/shared/core/aggregate.ts
@@ -1,26 +1,28 @@
 import Entity from "./entity";
-import DomainEvents from "./events";
 import ID from "./id";
 import { DomainEvent } from "./domain-event";
+import EventEmitter from "events";
 
 export class Aggregate<Props> extends Entity<Props> {
-	private _domainEvents: DomainEvents<this>;
+	private _domainEvents: DomainEvent[];
 
-	constructor(props: Props, id?: ID) {
+	constructor(props: Props, id?: ID) {		
 		super(props, id);
-		this._domainEvents = new DomainEvents(this);
+		this._domainEvents = [];
 	}
 
-	async dispatchAll(): Promise<void> {
-		await this._domainEvents.dispatchEvents();
+	async dispatchAll(emitter: EventEmitter = new EventEmitter()): Promise<void> {
+		this._domainEvents.forEach((event: DomainEvent) => {
+			emitter.emit(event.eventName, event);
+		});
 	};
 
 	clearEvents(): void {
-		this._domainEvents.clearEvents();
+		this._domainEvents = [];
 	};
 
 	addEvent(event: DomainEvent): void {
-		this._domainEvents.addEvent(event);
+		this._domainEvents.push(event);
 	}
 
 	toObject(): Props {
