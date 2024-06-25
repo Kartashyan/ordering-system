c8dc5b254babf5a457ca77e83199f74f3908c61d
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-25 23:59:14 +0200

    refactor: Update SignupUseCase to save user in the repository

--- src/users/application/signup.use-case.ts
+++ src/users/application/signup.use-case.ts
@@ -1,1 +1,14 @@
-export {}
+import { Result } from "../../shared";
+import { UserRepositoy } from "../domain/user-repo.port";
+import { User } from "../domain/user.aggregate";
+
+export class SignupUseCase {
+    constructor(userRepo: UserRepositoy) {
+        this.userRepo = userRepo;
+    }
+    async execute(command: any) {
+        const user = User.create(command.email, command.password);
+        await this.userRepo.save(user);
+        return Result.ok();
+    }
+}
