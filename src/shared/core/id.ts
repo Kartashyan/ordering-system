8137e68a81fc3f3bf6ed1af9ddcd9b2d32cefc9e
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-28 21:06:18 +0200

    refactor: Update ID class to use private fields

--- src/shared/core/id.ts
+++ src/shared/core/id.ts
@@ -1,52 +1,46 @@
 import { randomUUID } from './crypto';
 
 export class ID<T = string> {
-	private _value: string;
-	private _isNew: boolean;
-	private _createdAt: Date;
+	#value: string;
+	#isNew: boolean;
+	#createdAt: Date;
 
 	private constructor(id?: T) {
-		this._createdAt = new Date();
+		this.#createdAt = new Date();
 		if (typeof id === 'undefined') {
 			const uuid = randomUUID();
-			this._value = uuid;
-			this._isNew = true;
+			this.#value = uuid;
+			this.#isNew = true;
 			return this;
 		}
 		const isString = typeof id === 'string';
-		this._value = isString ? id as unknown as string : String(id);
-		this._isNew = false;
+		this.#value = isString ? id as unknown as string : String(id);
+		this.#isNew = false;
 		return this;
 	};
 
 	value(): string {
-		return this._value;
+		return this.#value;
 	}
 
 	isNew(): boolean {
-		return this._isNew;
+		return this.#isNew;
 	}
 
 	createdAt(): Date {
-		return this._createdAt;
+		return this.#createdAt;
 	}
 
 	equal(id: ID<any>): boolean {
-		return (typeof this._value === typeof id?.value()) && (this._value as any === id?.value());
+		return (typeof this.#value === typeof id?.value()) && (this.#value as any === id?.value());
 	}
 
 	isEqual(id: ID<any>): boolean {
 		return this.equal(id);
 	}
 
-	deepEqual(id: ID<any>): boolean {
-		const A = JSON.stringify(this);
-		const B = JSON.stringify(id);
-		return A === B;
-	}
-
 	clone(): ID<T> {
-		return new ID(this._value) as unknown as ID<T>;
+		return new ID(this.#value) as unknown as ID<T>;
 	}
 
 	public static create<T = string | number>(id?: T): ID<string> {
