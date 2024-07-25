73a67f9f74c4d5a578e07e460b5b04ca42b738b8
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-25 20:53:58 +0200

    refactor: Update UserCreatedEvent to include user object in constructor

--- src/users/domain/events/user-created.event.ts
+++ src/users/domain/events/user-created.event.ts
@@ -1,9 +1,12 @@
 import { DomainEvent } from "../../../shared/core/domain-event";
+import { User } from "../user.aggregate";
 
-export class UserCreatedEvent implements DomainEvent {
+export class UserCreatedEvent<User> implements DomainEvent<User> {
     readonly eventName: string = "UserCreated";
     readonly occurredOn: Date;
-    constructor() {
+    readonly aggregate: User;
+    constructor(user: User) {
         this.occurredOn = new Date();
+        this.aggregate = user;
     }
 }
