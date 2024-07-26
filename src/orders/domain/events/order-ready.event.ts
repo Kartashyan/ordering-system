4fad13d992da2c29177523f9a4bb7898f1073a9a
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-26 22:31:33 +0200

    refactor: Update OrderReadyEvent to include order object in constructor

--- src/orders/domain/events/order-ready.event.ts
+++ src/orders/domain/events/order-ready.event.ts
@@ -1,11 +1,10 @@
 import { DomainEvent } from "../../../shared/core/domain-event";
+import { Order } from "../order.aggregate";
 
-export class OrderReadyEvent implements DomainEvent {
+export class OrderReadyEvent implements DomainEvent<Order> {
   readonly eventName: string = "order-ready";
   readonly occurredOn: Date;
-  readonly aggregateId: string
-  constructor(aggregateId: string) {
+  constructor(public aggregate: Order) {
     this.occurredOn = new Date();
-    this.aggregateId = aggregateId;
   }
 }
